c6ccab0e8ebcccb5b0d50f7cbd973fd25de178dc
SahaHungkul <mhasbysyabani27@gmail.com>
2026-04-22 08:19:42 +0700

    feat: auth

--- Pinjamin-Aja/routes/api.php
+++ Pinjamin-Aja/routes/api.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function(){
     Route::post('/login',[AuthController::class,'login']);
+
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::get('/me', [AuthController::class,'me']);
+    });
 });
